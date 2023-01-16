29f8257d740fbfefef2f7e26e4ddb25aeea532b2
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2023-01-16 11:25:45 +0100

    add multiplication table function

---
+++ practice-multiplication-func.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var num, numOfTimes int
+
+	fmt.Println("Enter number to generate multiplication table")
+	fmt.Scanln(&num)
+
+	fmt.Println("Enter number of times the table should stop")
+	fmt.Scanln(&numOfTimes)
+
+	multiplyFunc(num, numOfTimes)
+	fmt.Println("Program run successfully")
+}
+
+func multiplyFunc(num, iteration int) {
+	for i := 1; i <= iteration; i++ {
+		fmt.Println(strconv.Itoa(num) + " " + "*" + " " + strconv.Itoa(i) + " " + "=", num * i)
+	}
+}
